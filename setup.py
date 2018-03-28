a5fcda2c3c699892003f842b8c711bd39f932b16
Akaame <Akaame@github.com>
2018-03-28 23:08:11 +0300

    Files and config for PyPI

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
 from distutils.core import setup
 
+url="https://github.com/Akaame/interval",
+version = "0.1.0"
+
 setup(name="Interval",
-version="0.1.0",
+version=version,
 description="Interval tree implementation in Python.",
 author="Siddik Acil",
-author_email="sddkacil.1@gmail.com",
+url=url,
+download_url=url+"/archive/"+version+".tar.gz", # There will be a download_url here
+author_email="sddkacil.1@gmail.com", 
 packages=["."]
 )
+
+# To get a download_url ->
+# Steps: 
+# git tag 0.1.0 -m "Add a tag for PyPI"
+# git push --tags origin master
